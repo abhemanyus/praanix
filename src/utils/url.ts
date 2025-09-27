f47c8f13dae7077218c0e81ed533ec6003eaf87a
Abhemanyu Sarin <abhemanyus@gmail.com>
2025-09-27 22:11:39 +0530

    stupid fix

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,4 +1,4 @@
 export function getAbsoluteUrl(path: string): string {
   const base = import.meta.env.BASE_URL == "/" ? "" : import.meta.env.BASE_URL || "";
-  return `${base}${path}`;
+  return `${base}${path}`.replace(/\/\//, '/');
 }
